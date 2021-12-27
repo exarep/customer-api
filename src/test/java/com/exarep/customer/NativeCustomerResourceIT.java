b093f4c4bbdd24f670bc44b5889a9c29296aec3d
Stephen Nimmo <stephennimmo@gmail.com>
2021-12-27 14:56:35 -0600

    Project init

---
+++ src/test/java/com/exarep/customer/NativeCustomerResourceIT.java
@@ -0,0 +1,9 @@
+package com.exarep.customer;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCustomerResourceIT extends CustomerResourceTest {
+
+    // Execute the same tests but in native mode.
+}
